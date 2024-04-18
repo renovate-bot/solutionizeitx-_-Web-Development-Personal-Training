9e6e9b4355c5051e4a6d9f346ea37102b438d2af
Raniyamohd <raniyamohd134@gmail.com>
2024-04-18 21:47:19 +0530

    task 5 task 11 task12 task 10 partially done

---
+++ Tasks/Task05/switch_day.js
@@ -0,0 +1,29 @@
+console.log("Program started")
+function day(){
+   var result;
+   day= document.getElementById("Myinput").value;
+   
+   
+   switch(day){
+    case "Monday":
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursday":
+    case "Friday":
+        result="Today is not weekend";
+    break;
+    case "Saturday":
+    case "Sunday":
+        result="Today is a weekend";
+    break;
+    default:
+        result="There is no such day";
+    
+   }
+   document.getElementById("output").innerHTML=result;
+}
+
+
+
+
+console.log("program ended")
