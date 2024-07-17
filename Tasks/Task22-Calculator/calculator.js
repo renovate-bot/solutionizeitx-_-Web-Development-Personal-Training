1c016e2206a44e9646ca508d25198b20fd3d2640
jasirmj <jasirmj@gmail.com>
2024-07-17 21:41:09 +0530

    space added

--- Tasks/Task22-Calculator/calculator.js
+++ Tasks/Task22-Calculator/calculator.js
@@ -1,14 +1,19 @@
 function fullClear() {
     document.getElementById('Myinput').value='';
-       
 }
+
+
 function deleteLast() {
     const display = document.getElementById('Myinput');
     display.value = display.value.slice(0, -1);
 }
+
+
 function appendCharacter(character) {
     document.getElementById('Myinput').value += character;
 }
+
+
 function calculate() {
     
     const display = document.getElementById('Myinput');
