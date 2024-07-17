ab944ed7a28958aaef8e71888ee35758f2dc6dac
jasirmj <jasirmj@gmail.com>
2024-07-17 21:43:40 +0530

    code formatted

--- Tasks/Task22-Calculator/calculator.js
+++ Tasks/Task22-Calculator/calculator.js
@@ -1,5 +1,5 @@
 function fullClear() {
-    document.getElementById('Myinput').value='';
+    document.getElementById('Myinput').value = '';
 }
 
 
@@ -15,56 +15,60 @@ function appendCharacter(character) {
 
 
 function calculate() {
-    
     const display = document.getElementById('Myinput');
-    
-    
-     display.value=eval(display.value);
-    }
+    display.value = eval(display.value);
+}
 
-    
-    
-    
-   
+
 function operator(operation) {
     const display = document.getElementById('Myinput');
     const value = parseFloat(display.value);
+
     if (isNaN(display.value)) {
         display.value = 'Error';
         return;
     }
+
     switch (operation) {
         case 'sqrt':
             display.value = Math.sqrt(value);
             break;
+
         case 'log':
             display.value = Math.log(value);
             break;
+
         case 'sin':
             display.value = Math.sin(value);
             break;
+
         case 'cos':
             display.value = Math.cos(value);
             break;
+
         case 'tan':
             display.value = Math.tan(value);
             break;
-         case 'sin_inverse':
-         display.value = Math.asin(value);
+
+        case 'sin_inverse':
+            display.value = Math.asin(value);
             break;
         case 'cos_inverse':
-        display.value = Math.acos(value);
+            display.value = Math.acos(value);
             break;
+
         case 'tan_inverse':
-        display.value = Math.atan(value);
+            display.value = Math.atan(value);
             break;
+
         case 'exp':
             display.value = Math.exp(value);
             break;
+            
         case 'reciprocal':
             display.value = 1 / value;
             break;
-        
+
         default:
             display.value = 'Error';
             break;
